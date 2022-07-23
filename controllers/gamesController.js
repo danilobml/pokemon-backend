4f0c80238c52a44e5bdfaba8f03be4a5c0a898f2
danilobml <danbarml@gmail.com>
2022-07-23 15:57:57 +0200

    mvp

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -19,7 +19,27 @@ const get_results = async (req, res) => {
   }
 };
 
+const get_winner_count = async (req, res) => {
+  try {
+    const winnerCount = await Game.aggregate([{ $group: { _id: "$winner", count: { $sum: 1 } } }, { $sort: { count: -1 } }]);
+    res.json(winnerCount);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// db.scores.aggregate([
+//   {
+//     $match: {
+//       winner: "Pikachu",
+//     },
+//   },
+//   {
+//     $count: "passing_scores",
+//   },
+// ]);
 module.exports = {
   save_game,
   get_results,
+  get_winner_count,
 };
--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const gamesRouter = express.Router();
-const { save_game, get_results } = require("../controllers/gamesController");
+const { save_game, get_results, get_winner_count } = require("../controllers/gamesController");
 
 gamesRouter.post("/save", save_game);
 
 gamesRouter.get("/leaderboard", get_results);
+
+gamesRouter.get("/leaderboard/counter", get_winner_count);
 
 module.exports = gamesRouter;
